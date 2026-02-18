23d369fd5a064c832600177e71635c2debe2f1c3
govindgcoder <govind.g.coder@gmail.com>
2026-02-18 20:44:01 +0530

    initial commit

--- src/ProducerConsumer.java
+++ src/ProducerConsumer.java
@@ -1,4 +1,5 @@
 class SharedResource {
+
     int item;
     boolean available = false;
 
@@ -7,36 +8,77 @@ class SharedResource {
     // set this.item = item, available = true
     // print "Produced: " + item
     // notify()
+    public synchronized void put(int item) {
+        try {
+            while (available) wait();
+        } catch (Exception e) {}
+        this.item = item;
+        this.available = true;
+        System.out.println("Produced: " + item);
+        notify();
+    }
 
     // TODO: synchronize void get()
     // while(!available) -> wait()
     // print "Consumed: " + item
     // available = false
     // notify()
+    public synchronized void get() {
+        try {
+            while (!available) wait();
+        } catch (Exception e) {}
+        System.out.println("Consumed: " + item);
+        this.available = false;
+        notify();
+    }
 }
 
 class Producer extends Thread {
+
     SharedResource resource;
+
     // TODO: Constructor to init resource
-    
+    public Producer(SharedResource resource) {
+        this.resource = resource;
+    }
+
     // TODO: run()
     // Loop 1 to 5
     // call resource.put(i)
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) resource.put(i);
+    }
 }
 
 class Consumer extends Thread {
+
     SharedResource resource;
+
     // TODO: Constructor to init resource
-    
+    public Consumer(SharedResource resource) {
+        this.resource = resource;
+    }
+
     // TODO: run()
     // Loop 1 to 5
-    // call resource.get()
+    // call resource.get(i)
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) resource.get();
+    }
 }
 
 public class ProducerConsumer {
+
     public static void main(String[] args) {
         // TODO: Create SharedResource object
+        SharedResource sr = new SharedResource();
         // TODO: Create Producer and Consumer threads
+        Producer pr = new Producer(sr);
+        Consumer cr = new Consumer(sr);
         // TODO: Start both threads
+        pr.start();
+        cr.start();
     }
 }
